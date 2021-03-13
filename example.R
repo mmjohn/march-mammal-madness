c333fa49c0b1289e3cc64f63b58fe74ef9ca4b94
mmjohn <mmjohn@utexas.edu>
2021-03-12 18:08:29 -0600

    Figured out way to store data

--- example.R
+++ example.R
@@ -1,1 +1,87 @@
-# scripts for scoring brackets
+# Possible scripts for scoring brackets
+# Mackenzie M Johnson
+# March 12, 2021
+
+library(tidyverse)
+
+# example bracket (mine)
+# track only winner ranks in order from top to bottom
+
+example_tricksy <- list(
+  tricksy_round1 = c(1, 8, 12, 4, 6, 14, 7, 2),
+  tricksy_round2 = c(1, 4, 6, 7),
+  tricksy_round3 = c(4, 7),
+  tricksy_winner = 4
+)
+
+example_red <- list(
+  red_round1 = c(16, 9, 5, 4, 11, 3, 7, 2),
+  red_round2 = c(9, 5, 3, 2),
+  red_round3 = c(9, 3),
+  red_winner = 3
+)
+
+example_myths <- list(
+  myths_round1 = c(16, 9, 5, 4, 6, 3, 10, 15),
+  myths_round2 = c(9, 4, 3, 10),
+  myths_round3 = c(9, 10),
+  myths_winner = 10
+)
+
+example_sea <- list(
+  sea_round1 = c(16, 9, 12, 13, 6, 14, 7, 15),
+  sea_round2 = c(),
+  sea_round3 = c(),
+  sea_winner = 
+)
+
+#### Wild Card
+
+#### Round 1 - Red, in Fur
+
+#### Round 1 - Tricksy Taxonomy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### Round 1 - Of Myths and Monsters
+
+#### Round 1 - Sea Beasties
+
+#### Round 2 - Red, in Fur
+
+#### Round 2 - Tricksy Taxonomy
+
+#### Round 2 - Of Myths and Monsters
+
+#### Round 2 - Sea Beasties 
+
+##### Sweet Sixteen
+
+#### Elite Trait
+
+#### Final Roar
+
+
+#### Championship
